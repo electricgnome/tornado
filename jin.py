f2e604973085f104fa0b05ff13026758e9f83bdb
electricgnome <hector@electricgnome.com>
2018-03-27 16:48:49 -0500

    minor rev

--- jin.py
+++ jin.py
@@ -10,22 +10,27 @@ ENV = Environment(
   loader=PackageLoader('myapp', 'templates'),
   autoescape=select_autoescape(['html', 'xml'])
 )
+
 class TemplateHandler(tornado.web.RequestHandler):
   def render_template (self, tpl, context):
     template = ENV.get_template(tpl)
     self.write(template.render(**context))
+
 class MainHandler(TemplateHandler):
   def get(self):
     self.set_header(
       'Cache-Control',
       'no-store, no-cache, must-revalidate, max-age=0')
-    self.render_template("hello.html", {'name': 'World'})
+    self.render_template("index.html", {'name': 'World'})
 
 
 
 def make_app():
   return tornado.web.Application([
     (r"/", MainHandler),
+    (r"/static/(.*)",
+    tornado.web.StaticFileHandler,
+    {'path': 'static'}),
   ], autoreload=True)
 
 if __name__ == "__main__":
